c126390848192af731f021383d5b13707f14492c
rafhael <rafhael.cancado@gmail.com>
2023-01-08 09:22:28 -0300

    [LNKR-2] post processing

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,5 +1,7 @@
 import { insertPost, selectPosts } from "../repositories/posts.repositories.js";
 import { postHashtags } from "./hashtags.controller.js";
+import urlMetadata from "url-metadata";
+import { getUserById } from "../repositories/users.repositories.js";
 
 export async function publishPost(req, res){
     const {url, content, userId} = res.locals.post;
@@ -15,12 +17,27 @@ export async function publishPost(req, res){
 export async function getPosts(req, res){
     const limit = parseInt(req.query.limit);                                                                                                                                                                                                                                                                                                                 
     try{
-        let {rows: posts} = await selectPosts();
-        posts = posts.reverse();
+        let {rows: postsRows} = await selectPosts();
+        postsRows = postsRows.reverse();
         if(limit>0){
-           posts = posts.slice(0,limit);
+           postsRows = postsRows.slice(0,limit);
         }
-        return res.status(200).send(posts);
+        for(let i = 0; i<postsRows.length; i++){
+            
+            const {rows: userRows} = await getUserById(postsRows[i].userid);
+            postsRows[i].userImage = userRows[0].pictureurl;
+            postsRows[i].username = userRows[0].username;
+            await urlMetadata(postsRows[i].url).then(
+                function (metadata) {
+                    postsRows[i].urlTitle = metadata.title;
+                    postsRows[i].urlDescription = metadata.description;
+                    postsRows[i].urlImage = metadata.image;
+                },
+                function (error) { // failure handler
+                    console.log(error)
+                });
+        }
+        return res.status(200).send(postsRows);
     }catch(error){
         return res.status(500).send(error.message);
     }
