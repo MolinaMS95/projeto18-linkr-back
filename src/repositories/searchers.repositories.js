07c53e9fea918c1d832571cb0f1ba66ad395f9b9
Renato da Silva Viana <verkylen@gmail.com>
2023-01-13 03:58:19 -0300

    [LNKR-6] refactor: return of search route

--- src/repositories/searchers.repositories.js
+++ src/repositories/searchers.repositories.js
@@ -8,7 +8,7 @@ export function getUsersByInitial(requesterid, initial) {
             FROM (
                 SELECT TRUE AS follows, users.id, users.username, users.pictureurl FROM users
             ) u1 FULL JOIN relationships ON u1.id = relationships.followedid
-            WHERE u1.username LIKE '${initial}%' AND relationships.followerid = $1
+            WHERE u1.username LIKE '%${initial}%' AND relationships.followerid = $1
         ) f FULL JOIN (
             SELECT 'key' AS key, JSON_AGG(u2) AS reminder
             FROM (
@@ -16,8 +16,8 @@ export function getUsersByInitial(requesterid, initial) {
                 ) u2 WHERE u2.id NOT IN (
                     SELECT users.id FROM users
                     FULL JOIN relationships ON users.id = relationships.followedid
-                    WHERE users.username LIKE '${initial}%' AND relationships.followerid = $1
-                ) AND u2.username LIKE '${initial}%'
+                    WHERE users.username LIKE '%${initial}%' AND relationships.followerid = $1
+                ) AND u2.username LIKE '%${initial}%'
         ) r ON f.key = r.key;
     `, [requesterid]);
 }
